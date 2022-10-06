95cbb3cd58a7e7a07d6f783c20e7fd4563044a95
dera273019 <Decebal.Radulescu@elektrobit.com>
2022-10-06 12:52:06 +0200

    Init dp-book gradle module

--- dp-book/src/main/java/org/uvt/DpBookMain.java
+++ dp-book/src/main/java/org/uvt/DpBookMain.java
@@ -1,6 +1,6 @@
-package org.example;
+package org.uvt;
 
-public class Main {
+public class DpBookMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
