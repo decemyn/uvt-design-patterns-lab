fcf3932677af9ce520cbc958012cbf1b7aa7135b
decemyn <decebal.radulescu02@e-uvt.ro>
2022-10-12 15:32:48 +0300

    Add main class

--- book/src/main/java/org/uvt/Main.java
+++ book/src/main/java/org/uvt/Main.java
@@ -2,6 +2,12 @@ package org.uvt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book myBook = new Book("TestBook");
+        myBook.createNewParagraph("Paragraph 1");
+        myBook.createNewParagraph("Paragraph 2");
+        myBook.createNewImage("Image 1");
+        myBook.createNewParagraph("Paragraph 3");
+        myBook.createNewTable("Table 1");
+        myBook.printBook();
     }
 }
