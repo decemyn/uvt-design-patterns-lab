c71e9ea1b8a0d8c0acfeebc74cd42d7138e8c927
decemyn <decebal.radulescu02@e-uvt.ro>
2022-10-27 13:40:27 +0300

    Implemented proxy design pattern for loading image

---
+++ book/src/main/java/org/uvt/book/ImageProxy.java
@@ -0,0 +1,42 @@
+package org.uvt.book;
+
+public class ImageProxy extends Element implements  IPicture {
+    Image realImage = null;
+    String url;
+    int dim;
+    String content;
+
+    public ImageProxy(String imageName) {
+        super(imageName);
+        this.url = imageName;
+        this.dim = this.url.length();
+        this.content = imageName;
+    }
+
+    // Extra image metadata (mock for now)
+
+    @Override
+    public String url() {
+        return this.url;
+    }
+
+    @Override
+    public int dim() {
+        return this.dim;
+    }
+
+    @Override
+    public String content() {
+        return this.content;
+    }
+
+    @Override
+    public void print() {
+        this.loadImage();
+        this.realImage.print();
+    }
+
+    public void loadImage() {
+        this.realImage = new Image(this.getMasterElement(), this.url, this.dim, this.content);
+    }
+}
